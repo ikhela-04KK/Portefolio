17c8e0f86ec0ffb349ea250518582fed0a69b1be
ikhela-04KK <102194271+ikhela-04KK@users.noreply.github.com>
2023-04-15 13:01:03 +0000

    add splash screen

---
+++ main-sps.js
@@ -0,0 +1,32 @@
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo-header");
+
+let logoSpan = document.querySelectorAll(".logo");
+
+window.addEventListener("DOMContentLoaded",()=>{
+  setTimeout(()=>{
+    logoSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+        span.classList.add('active');
+      },(idx+1)*400)
+    });
+
+    setTimeout(()=>{
+      logoSpan.forEach((span,idx)=>{
+
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.add('fade');
+        },(idx+1)*50)
+      })
+      
+    },2000);
+
+  
+    setTimeout(()=>{
+      intro.style.top="-140vh";
+      intro.style.transition = 'all 2s ease-in-out';
+    },4000);
+  
+  })
+})
